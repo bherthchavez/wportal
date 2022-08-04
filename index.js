95f314cb12c8cd9645ae5393d7762b1385372ee6
bherth <bherthchavez@gmail.com>
2022-08-04 19:45:44 +0800

    Adding journal module UI only  w/o functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/bankAccountDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/accountingDB", {useNewUrlParser: true});
 
 
 
@@ -20,6 +20,26 @@ const bank_accountsSchema = {
   current_balance: Number
 }
 const bank_Account = mongoose.model("bank_Account", bank_accountsSchema);
+
+const journal_EntrySchema = {
+  journalId: Number,
+  date: Date,
+  reference: String,
+  description: String,
+  created_at: Date
+};
+  
+const journal_Entry = mongoose.model("journal_Entry", journal_EntrySchema);
+
+const journal1 = new journal_Entry({
+  journalId: + 1,
+  date: Date.now(),
+  reference: "Payment for instalation",
+  description: "Payment first before setup",
+  created_at: Date.now()
+});
+
+
 const account1 = new bank_Account({
   bank_name: "Arab Bank",
   account_number: "65753213216",
@@ -44,32 +64,58 @@ const account3 = new bank_Account({
   current_balance: 17303750
 });
 
-
+const defaultJournalAccount = [journal1];
 const defaultBankAccount = [account1,account2,account3];
 
 app.get("/",function(req,res){
 res.render("index");
 });
 
+app.get("/view-journal",function(req,res){
+  res.render("view-journal");
+  });
 
-app.get("/manage-bank-accounts", function(req,res){
-  bank_Account.find({}, function(err, foundItems){
+app.get("/journal-accounts",function(req,res){
+  journal_Entry.find({}, function(err, foundItems){
     if (foundItems.length === 0) {
+      journal_Entry.insertMany(defaultJournalAccount, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+        }
+    });
+    res.redirect("journal-accounts");
+   } else {
+    res.render("journal-accounts", {journalAccounts: foundItems});
+  }
+  });
+  });
+
+app.get("/bank-accounts", function(req,res){
+
+  bank_Account.find({}, function(err, foundItems){
+      if (foundItems.length === 0){
       bank_Account.insertMany(defaultBankAccount, function(err){
         if (err) {
           console.log(err);
         } else {
-          console.log("Successfully savevd default items to DB.");
+          console.log("Successfully saved default items to DB.");
         }
       });
-      res.redirect("manage-bank-accounts");
+      res.redirect("bank-accounts");
     } else {
-      res.render("manage-bank-accounts", {bankAccount: foundItems});
+      res.render("bank-accounts", {bankAccount: foundItems});
     }
   });
 });
 
-app.post("/manage-bank-accounts", function(req, res){
+app.post("/view-journal", function(req,res){
+res.redirect("/view-journal");
+});
+
+
+app.post("/bank-accounts", function(req, res){
  
   const bankName = req.body.bankName;
   const accountNumber = req.body.accountNumber;
@@ -85,7 +131,7 @@ app.post("/manage-bank-accounts", function(req, res){
     current_balance: currentBalanace
   });
   account.save();
-  res.redirect("/manage-bank-accounts");
+  res.redirect("/bank-accounts");
 })
 
 
@@ -94,8 +140,7 @@ app.post("/deleteAccount", function(req,res){
 
   bank_Account.findByIdAndRemove(accountID, function(err){
     if (!err) {
-      console.log("Successfully deleted checked item.");
-      res.redirect("/manage-bank-accounts");
+      res.redirect("/bank-accounts");
     }
   });
 });
@@ -116,6 +161,12 @@ app.post("/viewAccount",function(req, res){
 
 
 app.get("/view-update-account", function(req,res){
+  
+  let format = (2500).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  console.log(format);
   res.render("view-update-account");
 })
 
@@ -125,12 +176,17 @@ app.post("/updateAccount", function(req,res){
   const bankName = req.body.bankName;
   const accountNumber = req.body.accountNumber;
   const accountType = req.body.accountType;
-  const currentBalance = parseFloat(req.body.currentBalance);
+  const currentBalance = req.body.currentBalance;
   const bankAddress = req.body.bankAddress;
   
-  bank_Account.findOneAndUpdate({_id: accountID}, {$set: {bank_name: bankName, account_number: accountNumber, account_type: accountType, bank_address: bankAddress, current_balance: currentBalance }}, function(err, foundList){
+  bank_Account.findOneAndUpdate({_id: accountID},
+     {$set: {bank_name: bankName, 
+      account_number: accountNumber, 
+      account_type: accountType, 
+      bank_address: bankAddress, 
+      current_balance: currentBalance }}, function(err, foundList){
     if (!err){
-      res.redirect("/manage-bank-accounts");
+      res.redirect("/bank-accounts");
     }else{
       console.log(err);
     }
