920485199c79a91203389b5682f1374eff974340
bherth <bherthchavez@gmail.com>
2022-08-29 01:38:43 +0800

    Update with complete Supplier and transactions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname +"/public"));
 
 app.use(session({
   secret: "secret",
@@ -30,6 +30,8 @@ const userSchema = new mongoose.Schema ({
   name: String,
   userRole: String,
   email: String,
+  created_at: Date,
+  username: String,
   password: String
 });
 
@@ -53,9 +55,76 @@ const bank_accountsSchema = {
   bank_email: String,
   deposited: Number,
   withdrawal: Number,
-  balance_amount: Number
+  balance_amount: Number,
+  created_at:Date
 }
-const bank_Account = mongoose.model("bank_Account", bank_accountsSchema);
+
+const bank_account = mongoose.model("bank_account", bank_accountsSchema);
+
+const supplier_accountsSchema = {
+  supplier_name: String,
+  a_name: String,
+  contact_person: String,
+  email: String,
+  billed: Number,
+  paid: Number,
+  balance_amount: Number,
+  created_at:Date
+}
+
+const supplier_account = mongoose.model("supplier_account", supplier_accountsSchema);
+
+const supplier_billSchema = {
+  supplier_id: String,
+  supplier_name: String,
+  bill_number: String,
+  cost_center: String,
+  bill_date: String,
+  due_date: String,
+  documents: String,
+  description: String,
+  total_payment: String,
+  total_items: String,
+  status: String,
+  items: Array,
+  items_description: Array,
+  items_price:Array,
+  items_qty:Array,
+  sub_total:Array,
+  created_by:String,
+  created_at:Date
+}
+
+const supplier_bill = mongoose.model("supplier_bill", supplier_billSchema);
+
+
+const payment_vouchersSchema = {
+  bank_id: String,
+  bank_name: String,
+  voucher_number: String,
+  payment_mode: String,
+  date: String,
+  documents: String,
+  description: String,
+  total_payment: String,
+  total_items: String,
+  status: String,
+  account_item: Array,
+  account_description: Array,
+  account_amount:Array,
+  created_by:String,
+  created_at:Date
+}
+
+const payment_voucher = mongoose.model("payment_voucher", payment_vouchersSchema);
+
+const chart_of_accountsSchema = {
+  name: String,
+  code: String,
+  created_at:Date
+}
+
+const chart_of_account = mongoose.model("chart_of_account", chart_of_accountsSchema);
 
 const journal_EntrySchema = {
   journalId: Number,
@@ -67,6 +136,70 @@ const journal_EntrySchema = {
   
 const journal_Entry = mongoose.model("journal_Entry", journal_EntrySchema);
 
+const chartOfAccount1 = new chart_of_account({
+  name: "Office Stationery",
+  code: "101",
+  created_at: Date.now()
+});
+
+const chartOfAccount2 = new chart_of_account({
+  name: "Petrol Expenses",
+  code: "102",
+  created_at: Date.now()
+});
+
+const chartOfAccount3 = new chart_of_account({
+  name: "Repair and Maintenance",
+  code: "103",
+  created_at: Date.now()
+});
+
+const chartOfAccount4 = new chart_of_account({
+  name: "Vehicle Insurance",
+  code: "104",
+  created_at: Date.now()
+});
+const chartOfAccount5 = new chart_of_account({
+  name: "Life Insurance",
+  code: "105",
+  created_at: Date.now()
+});
+const chartOfAccount6 = new chart_of_account({
+  name: "Ticket & Hotel Booking",
+  code: "106",
+  created_at: Date.now()
+});
+const chartOfAccount7 = new chart_of_account({
+  name: "Travel Insurance",
+  code: "107",
+  created_at: Date.now()
+});
+const chartOfAccount8 = new chart_of_account({
+  name: "Visa",
+  code: "108",
+  created_at: Date.now()
+});
+const chartOfAccount9 = new chart_of_account({
+  name: "Rent for Villa",
+  code: "109",
+  created_at: Date.now()
+});
+const chartOfAccount10 = new chart_of_account({
+  name: "Repair and Maintenance - Villa",
+  code: "110",
+  created_at: Date.now()
+});
+const chartOfAccount11 = new chart_of_account({
+  name: "Repair and Maintenance - Vihicle",
+  code: "111",
+  created_at: Date.now()
+});
+const chartOfAccount12 = new chart_of_account({
+  name: "Repair and Maintenance - Office",
+  code: "112",
+  created_at: Date.now()
+});
+
 const journal1 = new journal_Entry({
   journalId: + 1,
   date: Date.now(),
@@ -75,19 +208,42 @@ const journal1 = new journal_Entry({
   created_at: Date.now()
 });
 
+const supplier1 = new supplier_account({
+  supplier_name: "AC supplier",
+  a_name: "Arabic Name",
+  contact_person: "Mark Go",
+  email: "mark@acsupp.com",
+  billed: 0,
+  paid: 0,
+  balance_amount: 0,
+  created_at: Date.now()
+});
 
-const account1 = new bank_Account({
+const supplier2 = new supplier_account({
+  supplier_name: "Al Mana Vehicle Parts Supply",
+  a_name: "Arabic Name",
+  contact_person: "Glen Go",
+  email: "glen@amvps.com",
+  billed: 0,
+  paid: 0,
+  balance_amount: 0,
+  created_at: Date.now()
+});
+
+
+const account1 = new bank_account({
   bank_name: "Arab Bank",
   account_name: "DR. Nasser Abdulghani",
   account_number: "65753213216",
   account_type: "Current Account",
   bank_email: "jhon@arabbank.com",
-  deposited: 140300000,
+  deposited: 100.30,
   withdrawal: 0,
-  balance_amount: 140300000
+  balance_amount: 100.30,
+  created_at: Date.now()
 });
 
-const account2 = new bank_Account({
+const account2 = new bank_account({
   bank_name: "Industrial Credit Bank",
   account_name: "DR. Nasser Abdulghani",
   account_number: "216545432",
@@ -95,10 +251,11 @@ const account2 = new bank_Account({
   bank_email: "go@icb.com",
   deposited: 1706000,
   withdrawal: 0,
-  balance_amount: 1706000
+  balance_amount: 1706000,
+  created_at: Date.now()
 });
 
-const account3 = new bank_Account({
+const account3 = new bank_account({
   bank_name: "Doha bank",
   account_name: "Nafeesa Ahmed",
   account_number: "98198291",
@@ -106,14 +263,44 @@ const account3 = new bank_Account({
   bank_email: "mark@dohabank.com",
   deposited: 17303750,
   withdrawal: 0,
-  balance_amount: 17303750
+  balance_amount: 17303750,
+  created_at: Date.now()
 });
 
+const defaultChartAccount = [chartOfAccount1,
+  chartOfAccount2,
+  chartOfAccount3,
+  chartOfAccount4,
+  chartOfAccount5,
+  chartOfAccount6,
+  chartOfAccount7,
+  chartOfAccount8,
+  chartOfAccount9,
+  chartOfAccount10,
+  chartOfAccount11,
+  chartOfAccount12
+];
 const defaultJournalAccount = [journal1];
 const defaultBankAccount = [account1,account2,account3];
+const defaultSupplierAccount = [supplier1,supplier2];
 
-app.get("/",function(req,res){
+app.get("/",(req,res)=>{
+
+      // let dateNow = new Date();
+      // let name = "bherth";
+      // let userRole = "Admin";
+      // let userEmail="b@g.com";
+      // let username= "admin";
+      // let password="123";
+      // User.register({name: name, userRole: userRole, email: userEmail, created_at: dateNow, username: username},password, function(err, user){
+      //   if(err){
+      //     console.log(err);
+      //   }else{
+      //    console.log("success.");
+      //   }
+      // });
   
+
   if (req.isAuthenticated()){
     res.render("index", {userName: req.user.name, userRole: req.user.userRole});
    }else{
@@ -150,7 +337,8 @@ app.get("/sign-up",function(req,res){
   });
 
 app.post("/sign-up", function(req, res){
-  User.register({name: req.body.name, userRole: req.body.userRole, username: req.body.username},req.body.password, function(err, user){
+  let dateNow = Date.now;
+  User.register({name: req.body.name, userRole: req.body.userRole, email: req.body.userEmail, created_at: dateNow, username: req.body.username},req.body.password, function(err, user){
     if(err){
       console.log(err);
     }else{
@@ -158,6 +346,7 @@ app.post("/sign-up", function(req, res){
     }
   });
 });
+
 
 app.get('/logout', function(req, res){
   req.logout(function(err) {
@@ -189,13 +378,20 @@ app.get("/journal-accounts",function(req,res){
 
 app.get("/bank-accounts", function(req,res){
 
-  bank_Account.find({}, function(err, foundItems){
+  bank_account.find({}, function(err, foundItems){
       if (foundItems.length === 0){
-      bank_Account.insertMany(defaultBankAccount, function(err){
+      bank_account.insertMany(defaultBankAccount, function(err){
+            supplier_account.insertMany(defaultSupplierAccount, function(err){
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Successfully saved supplier default items to DB.");
+              }
+            });
         if (err) {
           console.log(err);
         } else {
-          console.log("Successfully saved default items to DB.");
+          console.log("Successfully saved bank default items to DB.");
         }
       });
       res.redirect("bank-accounts");
@@ -206,23 +402,206 @@ app.get("/bank-accounts", function(req,res){
   });
 });
 
-app.get("/create-voucher", function(req,res){
-res.render("payment-voucher",{userName: req.user.name, userRole: req.user.userRole, alert: alert});
-});
+app.get("/supplier-accounts", function(req,res){
 
-app.post("/create-voucher", function(req,res){
-const acountID =  req.body.accountID;
-
-  bank_Account.find({_id: accountID}, function(err, foundItems){
-    res.render("payment-voucher", {accountID:foundItems._id, bankName: foundItems.bank_name, ownerName: foundItems.acount_name});
+  supplier_account.find({}, function(err, foundItems){
+      if (foundItems.length === 0){
+     
+        supplier_account.insertMany(defaultSupplierAccount, function(err){
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Successfully saved supplier default items to DB.");
+              }
+            });
+      res.redirect("supplier-accounts");
+    } else {
+      res.render("supplier-accounts", {supplierAccount: foundItems, userName: req.user.name, userRole: req.user.userRole, alert: alert});
+    alert= 0;
+    }
   });
 });
 
+app.get("/create-voucher", (req,res) =>{
+res.render("payment-voucher",{userName: req.user.name, userRole: req.user.userRole, alert: alert});
+});
+
+
+app.post("/create-voucher", (req,res) =>{
+ 
+  chart_of_account.find({}, function(err, foundItems){
+    if (foundItems.length === 0){
+      chart_of_account.insertMany(defaultChartAccount, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+       
+        }
+      });
+    }else{
+          const accountID =  req.body.accountID;
+          bank_account.findOne({_id: accountID}, function(err, foundItem){
+        
+            res.render("payment-voucher", {chartAccounts: foundItems, 
+              accountID:foundItem._id,
+              bankName: foundItem.bank_name, 
+              ownerName: foundItem.account_name, 
+              userName: req.user.name, 
+              userRole: req.user.userRole });
+            
+          });
+    }
+  });
+});
+
+app.post("/create-supplier-bill", (req,res) =>{
+ 
+  chart_of_account.find({}, function(err, foundItems){
+    if (foundItems.length === 0){
+      chart_of_account.insertMany(defaultChartAccount, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+       
+        }
+      });
+    }else{
+      supplier_account.findOne({_id: req.body.accountID}, function(err, foundItem){
+        
+        res.render("create-supplier-bill", {chartAccounts: foundItems, 
+          accountID:foundItem._id,
+          supplierName: foundItem.supplier_name, 
+          aName: foundItem.a_name, 
+          userName: req.user.name, 
+          userRole: req.user.userRole });
+        
+      });
+    }
+  });
+});
+
+
+app.get("/voucher-item", (req,res) =>{
+  
+  payment_voucher.find({}, function(err, foundItems){
+    res.render("voucher-items",{voucherItems: foundItems, userName: req.user.name, userRole: req.user.userRole, alert: alert});
+  
+  });
+  });
+
+  app.post("/voucher-item", (req,res) =>{
+  
+    payment_voucher.findOne({_id: req.body.accountID}, function(err, foundItem){
+      if (err){
+        console.log(err);
+      }else{
+        res.render("voucher-items",{voucherItems: foundItem, userName: req.user.name, userRole: req.user.userRole, alert: alert});
+      }
+      
+    });
+    });
+
+
+
+app.post("/payment-voucher", function(req,res){
+  let totalBalance = 0;
+
+  bank_account.findOne({_id: req.body.accountID,}, function(err, foundItem){ 
+
+    totalBalance =  foundItem.balance_amount - req.body.totalPayment ;
+
+    bank_account.findOneAndUpdate({_id: req.body.accountID},
+      {$set: {
+       withdrawal: req.body.totalPayment,
+       balance_amount: totalBalance }}, function(err, foundList){
+     });
+
+  });
+
+// console.log(req.body);
+  const voucher = new payment_voucher({
+    bank_id:  req.body.accountID,
+    bank_name: req.body.pVbankName,
+    voucher_number: "PVR/2022/001",
+    payment_mode:  req.body.payment,
+    date:  req.body.date,
+    documents:  req.body.document,
+    description:  req.body.description,
+    total_payment: req.body.totalPayment,
+    total_items: req.body.numOfAcc,
+    status: "Pending",
+    account_item: req.body.account,
+    account_description: req.body.memo,
+    account_amount:req.body.amount,
+    created_by: req.user.name,
+    created_at: Date.now()
+  });
+  voucher.save();
+  res.redirect("/bank-accounts")
+
+});
+
+app.post("/supplier-bill", function(req,res){
+  // let totalBalance = 0;
+
+  // bank_account.findOne({_id: req.body.accountID,}, function(err, foundItem){ 
+
+  //   totalBalance =  foundItem.balance_amount - req.body.totalPayment ;
+
+  //   bank_account.findOneAndUpdate({_id: req.body.accountID},
+  //     {$set: {
+  //      withdrawal: req.body.totalPayment,
+  //      balance_amount: totalBalance }}, function(err, foundList){
+  //    });
+
+  // });
+
+console.log(req.body);
+  const bill = new supplier_bill({
+    supplier_id:  req.body.accountID,
+    supplier_name: req.body.supplierName,
+    bill_number: "2022/0001",
+    cost_center:  req.body.costCenter,
+    bill_date:  req.body.billDate,
+    due_date:  req.body.dueDate,
+    documents:  req.body.document,
+    description:  req.body.description,
+    total_payment: req.body.totalPayment,
+    total_items: req.body.numOfItem,
+    status: "Pending",
+    items: req.body.item,
+    items_description:  req.body.itemDesc,
+    items_price: req.body.price,
+    items_qty: req.body.qty,
+    sub_total: req.body.total,
+    created_by: req.user.name,
+    created_at: Date.now()
+
+  });
+  bill.save();
+  res.redirect("/supplier-accounts")
+
+});
 
 
 app.post("/view-journal", function(req,res){
   if (req.isAuthenticated()){
     res.redirect("/view-journal");
+  }else{
+    res.redirect("/sign-in");
+  }
+
+});
+
+app.get("/view-voucher",function(req,res){
+  res.render("view-voucher", {userName: req.user.name, userRole: req.user.userRole});
+  });
+
+app.post("/view-voucher", function(req,res){
+  if (req.isAuthenticated()){
+    res.redirect("/view-voucher");
   }else{
     res.redirect("/sign-in");
   }
@@ -240,7 +619,7 @@ app.post("/bank-accounts", function(req, res){
   const balanceAmount = parseFloat(req.body.openingBalance);
  
 
-  const account = new bank_Account({
+  const account = new bank_account({
     bank_name: bankName,
     account_name: ownerName,
     account_number: accountNumber,
@@ -248,7 +627,8 @@ app.post("/bank-accounts", function(req, res){
     bank_email: bankEmail,
     deposited: balanceAmount,
     withdrawal: 0,
-    balance_amount: balanceAmount
+    balance_amount: balanceAmount,
+    created_at: Date.now()
   });
   account.save();
   alert = 1;
@@ -258,11 +638,33 @@ app.post("/bank-accounts", function(req, res){
   }
 });
 
+app.post("/supplier-accounts", function(req, res){
+  if (req.isAuthenticated()){
+
+
+  const account = new supplier_account({
+    supplier_name: req.body.supplierName,
+    a_name: req.body.arabicName,
+    contact_personal: req.body.contactPerson,
+    email: req.body.supplierEmail,
+    billed: 0,
+    paid: 0,
+    balance_amount: 0,
+    created_at: Date.now()
+  });
+  account.save();
+  alert = 1;
+  res.redirect("/supplier-accounts");
+  }else{
+    res.redirect("/sign-in");
+  }
+});
+
 
 app.post("/deleteAccount", function(req,res){
   const accountID =  req.body.deleteAccount;
 
-  bank_Account.findByIdAndRemove(accountID, function(err){
+  bank_account.findByIdAndRemove(accountID, function(err){
     if (!err) {
       alert=2;
       res.redirect("/bank-accounts");
@@ -270,9 +672,20 @@ app.post("/deleteAccount", function(req,res){
   });
 });
 
+app.post("/deleteSupplier", function(req,res){
+  const accountID =  req.body.deleteAccount;
+
+  supplier_account.findByIdAndRemove(accountID, function(err){
+    if (!err) {
+      alert=2;
+      res.redirect("/supplier-accounts");
+    }
+  });
+});
+
 app.post("/viewAccount",function(req, res){
   const accountID = req.body.viewAccount;
-  bank_Account.findOne({_id: accountID}, function(err, foundList){
+  bank_account.findOne({_id: accountID}, function(err, foundList){
     
     res.render("view-update-account", {accountID:foundList._id ,
       bankName: foundList.bank_name, 
@@ -288,41 +701,33 @@ app.post("/viewAccount",function(req, res){
   });
 });
 
-
-
-app.get("/view-update-account", function(req,res){
-  
-    let format = (2500).toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD',
+app.post("/viewSuppplier",function(req, res){
+  const accountID = req.body.viewAccount;
+  supplier_account.findOne({_id: accountID}, function(err, foundList){
+    
+    res.render("view-supplier-account", {accountID:foundList._id ,
+      supplierName: foundList.supplier_name, 
+      arabicName: foundList.a_name,
+      contactPerson: foundList.contact_person,
+      email: foundList.email, 
+      bankEmail: foundList.bank_email,
+     userName: req.user.name, userRole: req.user.userRole,
+    
+      
     });
-    console.log(format);
-    res.render("view-update-account");
-
+  });
 });
 
 
 app.post("/updateAccount", function(req,res){
-  const accountID = req.body.accountID;
-  const bankName = req.body.bankName;
-  const ownerName = req.body.ownerName;
-  const accountNumber = req.body.accountNumber;
-  const accountType = req.body.accountType;
-  const bankEmail = req.body.bankEmail;
-  const deposited = req.body.deposited;
-  const withdrawal = req.body.withdrawal;
-  const balanceAmount = req.body.balanceAmount;
 
-
-  bank_Account.findOneAndUpdate({_id: accountID},
-     {$set: {bank_name: bankName,
-      account_name:  ownerName,
-      account_number: accountNumber, 
-      account_type: accountType, 
-      bank_email: bankEmail, 
-      deposited: deposited,
-      withdrawal: withdrawal,
-      balance_amount: balanceAmount }}, function(err, foundList){
+  bank_account.findOneAndUpdate({_id: req.body.accountID},
+     {$set: {bank_name:  req.body.bankName,
+      account_name:  req.body.ownerName,
+      account_number: req.body.accountNumber, 
+      account_type:  req.body.accountType, 
+      bank_email: req.body.bankEmail, 
+      deposited: req.body.deposited}}, function(err, foundList){
     if (!err){
       res.redirect("/bank-accounts");
     }else{
@@ -332,8 +737,25 @@ app.post("/updateAccount", function(req,res){
 
 });
 
+app.post("/updateSupplier", function(req,res){
+
+  supplier_account.findOneAndUpdate({_id: req.body.accountID},
+     {$set: {supplier_name:  req.body.supplierName,
+      a_name:  req.body.arabicName,
+      contact_person: req.body.contactPerson, 
+      email:  req.body.email}}, function(err, foundList){
+    if (!err){
+      res.redirect("/supplier-accounts");
+    }else{
+      console.log(err);
+    }
+  });
+
+});
+
 
 app.get("/users", function(req, res){
+
   if (req.isAuthenticated()){
 
     User.find({}, function(err, foundItems){
